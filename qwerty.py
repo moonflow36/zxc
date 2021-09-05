88fd25215d45937fc60cf1720efb83bb65ea2c02
moonflow36 <pavlinov-2006@mail.ru>
2021-09-05 13:33:26 +0700

    21 pishu

--- qwerty.py
+++ qwerty.py
@@ -1,6 +1,26 @@
-a = int(input())
-if a > 5:
-    print(a ** 2)
-else:
-    print(f"Loh")
-#tgbtgbtgb
+import random
+v = 2
+k = 4
+d = 3
+t = 11
+b = 0
+c = 21
+n = [2, 3, 4, 5, 6, 7, 8, 9, 10, "v", "k", "d", "t"]
+while True:
+    b = 0
+    print(f"Вам выполо: {random.sample(n,3)}")
+    a = int(input("Napishite chislo: "))
+    b += a
+    if b < 21:
+        print(f"Вам выполо: {random.sample(n,3)}")
+        a = int(input("Napishite chislo: "))
+        b += a
+    
+    if b == 21:
+        break
+        
+    if b > 21:
+        break
+        
+
+
