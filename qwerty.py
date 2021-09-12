febd966accd783beb84603ccafac2b347e08a026
moonflow36 <90123042+moonflow36@users.noreply.github.com>
2021-09-12 10:37:22 +0700

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,26 +1,33 @@
 import random
-v = 2
-k = 4
-d = 3
-t = 11
-b = 0
-c = 21
-n = [2, 3, 4, 5, 6, 7, 8, 9, 10, "v", "k", "d", "t"]
-while True:
-    b = 0
-    print(f"Вам выполо: {random.sample(n,3)}")
-    a = int(input("Napishite chislo: "))
-    b += a
-    if b < 21:
-        print(f"Вам выполо: {random.sample(n,3)}")
-        a = int(input("Napishite chislo: "))
-        b += a
+
+
+def create_deck():
+    deck = []
+    for i in range(2, 11):
+        for j in ('C', 'P', 'B', 'X'):
+            deck.append(str(i)+j)
+    for i in ('J','Q','K', 'T'):
+        for j in ('C', 'P', 'B', 'X'):
+            deck.append(i+j)
+    return deck
+
+
+def started_hand(deck):
+    hand = []
+    for i in range(2):
+        hand.append(random.choice(deck))
+        deck.remove(hand[-1])
+    return hand, deck
     
-    if b == 21:
-        break
-        
-    if b > 21:
-        break
+
+
+def start_game():
+    deck = create_deck()
+    hand = []
+    hand, deck = started_hand(deck)
+    
+
+start_game()
         
 
 
