ac7282a712497fa99a1786050c75899d991011d1
moonflow36 <90123042+moonflow36@users.noreply.github.com>
2021-09-12 12:51:44 +0700

    Update qwerty.py

--- qwerty.py
+++ qwerty.py
@@ -1,33 +1,25 @@
 import random
+tuz = 11
+valet = 2
+korol = 4
+dama = 3
+z = ["tuz(11)", "valet(2)", "korol(4)", "dama(3)"]
+a = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+b = 0
+while True:
+    s = random.sample(a, random.randint(1, 3))
+    if len(s) < 3:
+        s.extend(random.sample(z, 3 - len(s)))
+    print(f"Введите число которое выбрали {s}") 
+    m = int(input())
+    b += m
+    if b == 21:
+        print("You winner")
+        break
+    elif b > 21:
+        print(f"You have {b} points and you lose")
+        break
 
-
-def create_deck():
-    deck = []
-    for i in range(2, 11):
-        for j in ('C', 'P', 'B', 'X'):
-            deck.append(str(i)+j)
-    for i in ('J','Q','K', 'T'):
-        for j in ('C', 'P', 'B', 'X'):
-            deck.append(i+j)
-    return deck
-
-
-def started_hand(deck):
-    hand = []
-    for i in range(2):
-        hand.append(random.choice(deck))
-        deck.remove(hand[-1])
-    return hand, deck
-    
-
-
-def start_game():
-    deck = create_deck()
-    hand = []
-    hand, deck = started_hand(deck)
-    
-
-start_game()
         
 
 
